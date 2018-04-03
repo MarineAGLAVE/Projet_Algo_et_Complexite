ba22ff81fdac610756f0aecf72ea9147e0b6f558
MarineAGLAVE <38038917+MarineAGLAVE@users.noreply.github.com>
2018-04-03 21:54:24 +0200

    Add files via upload

--- Aide_Projet_Algo_et_Compexite.c
+++ Aide_Projet_Algo_et_Compexite.c
@@ -93,12 +93,114 @@ UPGM(une matrice de distances M de taille n×n){
 	retourner arbre;
 	
 	FIN
+// STRUCTURES A MODIF ==================================================
+// TP2:
+typedef struct Maillon Maillon;
+typedef struct Liste Liste;
+
+struct Maillon {
+    Maillon *suivant;
+    int valeur;
+};
+
+struct Liste {
+    Maillon *tete;
+};
+//TP3:
+typedef struct Noeud Noeud;
+
+struct Noeud {
+    int cle;
+    struct Noeud *gauche;
+    struct Noeud *droit;
+};
+
+typedef struct Arbre {
+    Noeud *racine;
+} Arbre;
+
+// INCLUDE A FAIRE ===================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "liste.h"
+
 // FONCTIONS A FAIRE ===================================================
 
-nouvel_arbre (TP3)
-nouveau_noeud (TP3)
-nouvelle_liste (TP2)
-ajout_element_fin (TP2)
-supp_element (TP2) (à adapter pour que  lespositions commencent à 0 et non à 1)
-Element (L:liste chaînée (IN), i:entier (IN), OUT: noeud pointé par iième maillon de L ou NULL s'il n'existe pas)
-mise_a_jour (M: matrice de distance(IN), OUT: nouvelle matrice de distance)
+//nouvel_arbre (TP3)
+/* Cree un nouvel arbre binaire et renvoie un pointeur vers cet arbre */
+Arbre *nouvel_arbre() {
+    Arbre *arbre=(Arbre *) malloc(sizeof(arbre));
+    if(arbre==NULL)     //si pas d'arbre
+    {
+        fprintf(stderr,"ERREUR ALLOCATION ARBRE.\n");
+        exit(1);
+    }
+    arbre->racine=NULL;
+    return arbre;
+}
+// ===================================================
+//nouveau_noeud (TP3)
+/* Cree un nouveau noeud et renvoie un pointeur vers ce noeud */
+Noeud *nouveau_noeud(int val) {
+    
+    Noeud *noeud=(Noeud *) malloc(sizeof(noeud));
+    if(noeud==NULL)     //if pas de noeud
+    {
+        fprintf(stderr,"ERREUR ALLOCATION NOEUD.\n");
+        exit(1);
+    }
+    noeud->gauche=NULL;
+    noeud->droit=NULL;
+    noeud->cle=val;
+    
+    return noeud;
+}
+
+// ===================================================
+//nouvelle_liste (TP2)
+/* crée une nouvelle liste */
+Liste *nouvelle_liste() {
+    Liste *liste=(Liste *) malloc(sizeof(liste));
+    if(liste==NULL)     //if(!liste)
+    {
+        fprintf(stderr,"ERREUR ALLOCATION LISTE.\n");
+        exit(1);
+    }
+    liste->tete=NULL;
+    return liste;
+}
+
+// ===================================================
+//ajout_element_fin (TP2)
+/* ajoute un élément de valeur val à la fin de la liste */
+void ajout_element_fin(Liste *L, int val) {
+    if(L==NULL)return;    //void ne retourne rien, mais "return" permet de ne pas continuer la fonction si la liste est déjà vide
+    Maillon *q=nouveau_maillon(val);
+    if (L->tete== NULL){
+        L->tete = q;
+    }    
+    else{
+        Maillon *p=L->tete;
+        while(p->suivant!=NULL)      //tant que p-suivant non NULL (donc pas arrivé à la fin)
+        {
+            p=p->suivant;
+        }
+        p->suivant=q;
+        
+    }
+}
+
+// ===================================================
+//supp_element (TP2) (à adapter pour que  lespositions commencent à 0 et non à 1)
+/* supprime le k-ième élément de la liste (on suppose que la première place est
+ * 1); si c'est possible, on modifie la liste et on renvoie la valeur 1; sinon,
+ * on ne modifie pas la liste et on renvoie la valeur 0 */
+int supp_element(Liste *L, int k) {
+    return 0;
+}
+
+// ===================================================
+//Element (L:liste chaînée (IN), i:entier (IN), OUT: noeud pointé par iième maillon de L ou NULL s'il n'existe pas)
+// ===================================================
+//mise_a_jour (M: matrice de distance(IN), OUT: nouvelle matrice de distance)
